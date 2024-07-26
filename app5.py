ad9c4edda4331085f75ca7a3f5b9d2044ae9dbf1
Mulweli Mudau <mudaumulweliray@gmail.com>
2024-07-26 17:36:18 +0200

    Add files via upload

---
+++ app5.py
@@ -0,0 +1,68 @@
+import os
+
+
+
+import bs4
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+load_dotenv()
+#### INDEXING ####
+import streamlit as st
+
+
+st.title('Dating Assistant')
+question=st.text_input('Well, you are single right?.. make your family proud and get a girlfriend, come on bro. Ask me anything approaching girls')
+
+#url="https://www.ibm.com/topics/natural-language-processing/"
+url="https://www.mantelligence.com/how-to-approach-a-girl/"
+
+# Load Documents
+loader = WebBaseLoader(
+    web_paths=(url,),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+# Embed
+vectorstore = FAISS.from_documents(documents=splits,
+                                    embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
+
+#### RETRIEVAL and GENERATION ####
+
+# Prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+# LLM
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.4)
+
+# Post-processing
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Question
+results=rag_chain.invoke(question)
+st.write(results)
